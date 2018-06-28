e9d4e7b1480e19ac757c3ecdedbc315dc59ca111
Kara Stubbs <karaestubbs@gmail.com>
2018-06-29 00:15:57 +0100

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+        host: 'cdn.contentful.com',
+      },
+    },
+  ],
+}
